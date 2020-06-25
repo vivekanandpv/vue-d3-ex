573b7224f8346616c8b0606f513fd9c7d131faae
Vivekanand P V <vivekanandpv@outlook.com>
2020-06-25 17:55:13 +0530

    done

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,3 +1,17 @@
 <template>
-  <h3>About Page</h3>
+  <div>
+    <h3>About Page</h3>
+    <hr />
+    <p>We got: {{ queryParams }}</p>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      queryParams: this.$route.query,
+    };
+  },
+};
+</script>
