0083ec633b0513c8aeb26d24a81a1ee2a48f0400
Vivekanand P V <vivekanandpv@outlook.com>
2020-06-25 17:43:24 +0530

    done

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,17 @@
 <template>
   <div id="app">
     <app-navbar></app-navbar>
-    <router-view></router-view>
+    <div class="container my-4">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-import Navabar from "./components/Navbar";
+import Navabar from './components/Navbar';
 export default {
   components: {
-    "app-navbar": Navabar
-  }
+    'app-navbar': Navabar,
+  },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,52 @@
 <template>
   <div>
-    <router-link to="/" exact>
-      <h4>Home</h4>
-    </router-link>
-    <br />
-    <router-link to="/about" exact>About</router-link>
-    <br />
-    <router-link to="/news" exact>News</router-link>
-    <br />
-    <router-link to="/news/Chennai/2019" exact>News with Params</router-link>
-    <br />
-    <router-link to="/contact" exact>Contact</router-link>
-    <br />
-    <router-link to="/djddjdjd" exact>Gibberish</router-link>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">
+        <img
+          src="../assets/logo.png"
+          width="30"
+          height="30"
+          class="d-inline-block align-top"
+          alt=""
+          loading="lazy"
+        />
+        Vue.js Learning
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <router-link to="/" exact class="nav-link r-link">
+              <span>Home</span>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/about" exact class="nav-link">
+              <span>About</span>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/news" exact class="nav-link">
+              <span>News</span>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/contact" exact class="nav-link">
+              <span>Contact</span>
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </div>
 </template>
