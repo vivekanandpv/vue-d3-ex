7d3db6eb3f868e5a07a8f6b8179db58180258da3
Vivekanand P V <vivekanandpv@outlook.com>
2020-06-25 14:56:16 +0530

    ready for exercise

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
-    <router-link to="/" exact>Home</router-link>
+    <router-link to="/" exact>
+      <h4>Home</h4>
+    </router-link>
     <br />
     <router-link to="/about" exact>About</router-link>
     <br />
     <router-link to="/news" exact>News</router-link>
+    <br />
+    <router-link to="/news/Chennai/2019" exact>News with Params</router-link>
     <br />
     <router-link to="/contact" exact>Contact</router-link>
     <br />
--- src/components/News.vue
+++ src/components/News.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
     <h3>News Page</h3>
+    <p>City: {{city}}, year: {{year}}</p>
+    <p>Params: {{params}}</p>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      city: this.$route.params.city,
+      year: this.$route.params.year,
+      params: this.$route.params
+    };
+  }
+};
+</script>
