87c4dab831d0b9f231066a697e9c8ee9d2cfe6e5
Vivekanand P V <vivekanandpv@outlook.com>
2020-06-25 14:27:15 +0530

    router-links

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,3 +1,11 @@
 <template>
-  <h3>Navbar</h3>
+  <div>
+    <router-link to="/" exact>Home</router-link>
+    <br />
+    <router-link to="/about" exact>About</router-link>
+    <br />
+    <router-link to="/news" exact>News</router-link>
+    <br />
+    <router-link to="/contact" exact>Contact</router-link>
+  </div>
 </template>
