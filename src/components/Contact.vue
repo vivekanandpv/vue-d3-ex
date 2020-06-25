b98cf2db4b8a965bad837fe209c34e0389db7522
Vivekanand P V <vivekanandpv@outlook.com>
2020-06-25 17:47:47 +0530

    done

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,3 +1,17 @@
 <template>
-  <h3>Contact Page</h3>
+  <div>
+    <h3>Contact Page</h3>
+    <hr />
+    <button class="btn btn-primary" @click="navigate">Go to About</button>
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    navigate() {
+      this.$router.push({ path: '/about', query: { x: 100, y: 'India' } });
+    },
+  },
+};
+</script>
