73ce064b992e83af77ef57d2d9b546448297dc99
Vivekanand P V <vivekanandpv@outlook.com>
2020-06-25 14:04:05 +0530

    customizing the file structure

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import routes from './routes/routes';
+import VueRouter from 'vue-router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes,
+});
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
---
+++ src/routes/routes.js
@@ -0,0 +1,10 @@
+import Home from '../components/Home.vue';
+
+const routes = [
+  {
+    path: '/',
+    component: Home,
+  },
+];
+
+export default routes;
