d11776db624454ce29a8d063098c27c330fa2ca1
Vivekanand P V <vivekanandpv@outlook.com>
2020-06-25 19:05:20 +0530

    done

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -5,20 +5,31 @@
     <form>
       <div class="form-group">
         <label>Name</label>
-        <input type="text" class="form-control" />
+        <input type="text" class="form-control" v-model="form.name" />
       </div>
       <div class="form-group">
         <label>Phone number</label>
-        <input type="text" class="form-control" />
+        <input type="text" class="form-control" v-model="form.phone" />
       </div>
       <div class="form-group form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+        <input
+          type="checkbox"
+          class="form-check-input"
+          id="exampleCheck1"
+          v-model="form.newsletter"
+        />
         <label class="form-check-label" for="exampleCheck1">Newsletter</label>
       </div>
       <div class="form-group">
         <label>Language</label>
-        <select class="form-control">
-          <option>English</option>
+        <select class="form-control" v-model="form.language">
+          <option value="">---Please Select---</option>
+          <option
+            v-for="(lang, index) in languages"
+            v-bind:key="index"
+            :value="lang"
+            >{{ lang }}</option
+          >
         </select>
       </div>
       <button class="btn btn-primary mt-4">Submit</button>
