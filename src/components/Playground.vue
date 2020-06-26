47722a4a17a20b9cac95e451e67bfbc5eae2c4f8
Vivekanand P V <vivekanandpv@outlook.com>
2020-06-26 13:01:41 +0530

    done

--- src/components/Playground.vue
+++ src/components/Playground.vue
@@ -1,19 +1,42 @@
 <template>
   <div>
     <h3>Playground: Users</h3>
-    <hr />
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Email</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(user, index) in users" :key="index">
+          <td>{{ user.id }}</td>
+          <td>{{ user.email }}</td>
+          <td>{{ user.first_name }}</td>
+          <td>{{ user.last_name }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
   async created() {
     try {
       //  since the http client is configured for global injector,
       //  we can use it as part of every child
-      const data = await this.$http.get(`users?page=2`);
-      console.log('Server', data);
+      const response = await this.$http.get(`users?page=2`);
+      this.users = response.data.data;
     } catch (error) {
+      alert('Oops! there was a problem!');
       console.log('Error', error);
     }
   },
